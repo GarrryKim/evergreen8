a7a5a1661d61164ea8da8e4fa00276523d5289c1
김재훈 <kgarry@naver.com>
2024-08-31 22:59:26 +0900

    Design : Add DuckHunt Background

---
+++ evergreen/Assets/DuckHunt/DuckHuntIntro.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckHuntIntro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("오리를 잡자!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
